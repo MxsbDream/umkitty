0a26aaeae3d68566b93d788c4646dbe7d89acebf
Minedream • Développeur qui a de la passion pour le dev (beaucoup) • Français <naelperdu@gmail.com>
2025-11-21 16:00:05 +0100

    Create setup.js

---
+++ .kitdev/setup.js
@@ -0,0 +1,11 @@
+install kitdev-scripter.pkg
+sign={only in={setup} id="01&" sign="."}
+setup={repo=github/MxsbDream/umkitty/01&kitdev}
+{ kitdev 
+setup
+  }
+{ kitdev
+ file-ext
+ setup={name=kdv}
+ info={link="https://github.com/MxsbDream/umkitty/.kitdev/setup/file-ext/KDVext"}
+   }
